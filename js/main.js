d906cfed7c556d0c4c1ecba8419415e7d921cb63
developernahian <developer.nahian@gmail.com>
2024-09-25 02:49:37 +0600

    final update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// TODO: Donate section Card One
+// TODO: Donate Card One Start
 document
   .getElementById("donate-btn-one")
   .addEventListener("click", function () {
@@ -6,8 +6,6 @@ document
     const totalDonateOne = getTotalAmmountById("total-donate-one");
 
     const balance = getTotalAmmountById("balance");
-
-    console.table({ donateInputOne, totalDonateOne });
 
     if (
       donateInputOne < 1 ||
@@ -20,7 +18,6 @@ document
     }
 
     const newTotalDonateOne = totalDonateOne + donateInputOne;
-    console.log(newTotalDonateOne);
 
     document.getElementById("total-donate-one").innerText = newTotalDonateOne;
 
@@ -31,18 +28,16 @@ document
     const newBalance = balance - donateInputOne;
     document.getElementById("balance").innerText = newBalance;
 
-    //TODO: History add for 1st card start
+    //todo History add for 1st card
 
     getId("history-container").innerHTML += showHistory(
       donateInputOne,
       "Taka is Donated for famine-2024 at Noakhali, Bangladesh",
       currentDate
     );
-
-    //todo END
   });
 
-// TODO: Donate Section Card Two
+// TODO: Donate Card Two Start
 document
   .getElementById("donate-btn-two")
   .addEventListener("click", function () {
@@ -50,8 +45,6 @@ document
     const totalDonateTwo = getTotalAmmountById("total-donate-two");
 
     const balance = getTotalAmmountById("balance");
-
-    console.log(donateInputTwo, totalDonateTwo);
 
     if (
       donateInputTwo < 1 ||
@@ -66,7 +59,6 @@ document
     my_modal_5.showModal();
 
     const newTotalDonateTwo = totalDonateTwo + donateInputTwo;
-    console.log(newTotalDonateTwo);
 
     document.getElementById("total-donate-two").innerText = newTotalDonateTwo;
 
@@ -75,17 +67,15 @@ document
     const newBalance = balance - donateInputTwo;
     document.getElementById("balance").innerText = newBalance;
 
-    //TODO: History add for 2nd card start
+    //todo History add for 2nd card
 
     getId("history-container").innerHTML += showHistory(
       donateInputTwo,
       "Taka is Donated for Flood Relief in Feni, Bangladesh"
     );
-
-    //todo END
   });
 
-// TODO: Donate Section Card Three
+// TODO: Donate Card Three Start
 document
   .getElementById("donate-btn-three")
   .addEventListener("click", function () {
@@ -93,8 +83,6 @@ document
     const totalDonateThree = getTotalAmmountById("total-donate-three");
 
     const balance = getTotalAmmountById("balance");
-
-    console.log(donateInputThree, totalDonateThree);
 
     if (
       donateInputThree < 1 ||
@@ -107,7 +95,6 @@ document
     }
 
     const newTotalDonateThree = totalDonateThree + donateInputThree;
-    console.log(newTotalDonateThree);
 
     document.getElementById("total-donate-three").innerText =
       newTotalDonateThree;
@@ -119,23 +106,19 @@ document
     const newBalance = balance - donateInputThree;
     document.getElementById("balance").innerText = newBalance;
 
-    //TODO: History add for 3nd card start
+    //todo History add for 3rd card
 
     getId("history-container").innerHTML += showHistory(
       donateInputThree,
       "Taka is Donated for Injured in the Quota Movement, Bangladesh"
     );
-
-    //todo END
   });
 
-//TODO: history and donation tab and button functionality
+//TODO: history and donation button functionality Start
 const donationButton = document.getElementById("donation-button");
 const historyButton = document.getElementById("history-button");
 
 donationButton.addEventListener("click", function () {
-  console.log("Donation button clicked");
-
   document.getElementById("history-section").classList.add("hidden");
   document.getElementById("donation-section").classList.remove("hidden");
 
@@ -145,8 +128,6 @@ donationButton.addEventListener("click", function () {
 });
 
 historyButton.addEventListener("click", function () {
-  console.log("History Button clicked");
-
   document.getElementById("history-section").classList.remove("hidden");
   document.getElementById("donation-section").classList.add("hidden");
 
